d58dc2bbdb95c0cc4fe8938749d94d78e2c423aa
Isah <isaaholiveira15@gmail.com>
2024-12-09 21:31:37 -0300

    feat: remover o ssr

--- projects/mfe-noticias/src/app/profile/profile.module.ts
+++ projects/mfe-noticias/src/app/profile/profile.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     ProfileComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ProfileComponent
+      }
+    ])
   ]
 })
 export class ProfileModule { }
